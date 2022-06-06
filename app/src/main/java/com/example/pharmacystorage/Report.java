de55f5fdab1d77fd375612fece4fffd6ab262edd
Влад Влад <vladis.a.-01@mail.ru>
2022-06-06 13:16:09 +0400

    бесполезный комм

--- app/src/main/java/com/example/pharmacystorage/Report.java
+++ app/src/main/java/com/example/pharmacystorage/Report.java
@@ -1,6 +1,7 @@
 package com.example.pharmacystorage;
 
 import android.os.Environment;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
@@ -10,6 +11,7 @@ public class Report {
     public void generatePdf(Date dateFrom, Date dateTo) throws IOException {
         String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString();
         File file = new File(pdfPath, "report.pdf");
+
 
     }
 
